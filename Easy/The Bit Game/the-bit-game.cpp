cea9c092546e558613e4eeb64fc09b367d991478
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-21 10:18:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/The Bit Game/the-bit-game.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int swapBitGame(long long N){
+        // code here 
+         int ct = __builtin_popcountll(N);
+        if(ct & 1) return 1;
+        return 2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.swapBitGame(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
