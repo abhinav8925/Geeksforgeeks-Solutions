8c753c8991a1f416d56382cb14ffe5948c42b00e
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-09-21 10:35:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of paths/number-of-paths.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+long long  numberOfPaths(int m, int n)
+{
+    // Code Here
+     if(n==1||m==1){
+            return 1;
+        }
+        return numberOfPaths(n,m-1)+numberOfPaths(n-1,m);
+}
+
+//{ Driver Code Starts.
+
+ 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,m;
+		cin>>m>>n;
+	    cout << numberOfPaths(m, n)<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
