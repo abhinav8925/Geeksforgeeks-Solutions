d50841e44c2e63334ffc4cc28c9eaf78d6d9ec9e
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-10-19 22:44:12 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/N-Queen Problem/nqueen-problem.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    
+    bool isSafe(int row,int col,vector<vector<int>> &board,int n){
+        
+        int x = row,y=col;
+        
+        while(y>=0){
+            if(board[x][y]==1)
+                return false;
+         y--;       
+        }
+        int digx=row,digy=col;
+        
+        while(digx>=0 && digy>=0){
+            if(board[digx][digy]==1)
+                return 0;
+            digx--;
+            digy--;
+        }
+        digx=row,digy=col;
+        
+        while(digx<n && digy>=0){
+            if(board[digx][digy]==1)
+                return 0;
+            digx++;
+            digy--;
+            
+        }
+        return 1;
+    }
+    void solve(int col,vector<vector<int>> &ans,vector<vector<int>> &board,vector<int> &temp,int n){
+        
+        if(col == n){
+            ans.push_back(temp);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,board,n)){
+                temp.push_back(row+1);
+                board[row][col]=1;
+                solve(col+1,ans,board,temp,n);
+                board[row][col]=0;
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> nQueen(int n) {
+        // code here
+        
+        vector<vector<int>> board(n,vector<int> (n,0));
+        vector<vector<int>> ans;
+        vector<int> temp;
+        
+        solve(0,ans,board,temp,n);
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        Solution ob;
+        vector<vector<int>> ans = ob.nQueen(n);
+        if(ans.size() == 0)
+            cout<<-1<<"\n";
+        else {
+            for(int i = 0;i < ans.size();i++){
+                cout<<"[";
+                for(int u: ans[i])
+                    cout<<u<<" ";
+                cout<<"] ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
