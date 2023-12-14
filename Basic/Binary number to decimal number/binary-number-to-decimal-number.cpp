0075562ef783a8a1844484b5470c4f1cd3cde8cd
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-12-14 23:03:03 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Binary number to decimal number/binary-number-to-decimal-number.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int binary_to_decimal(string str) {
+        // Code here.
+         int dec = 0;
+    for (int i = 0; i < str.size(); i++)
+    {
+        dec <<= 1;
+        dec ^= str[i] ^ 48;
+    }
+    return dec;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        int ans = ob.binary_to_decimal(str);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
