fe88d2dbc6051153c4d66ff35611b134277717b7
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-12-31 15:12:31 +0530

    Updated Solution - GfG to GitHub

--- Medium/New Year Resolution/new-year-resolution.cpp
+++ Medium/New Year Resolution/new-year-resolution.cpp
@@ -17,17 +17,24 @@ public:
         
         if(sum && (sum%20==0 || sum%24==0 || sum==2024))
                 return 1;
+                
         
         if(dp[ind][sum]!=-1)
             return dp[ind][sum];
+            
         bool t = f(ind+1,n,sum+coins[ind],coins,dp);
+        
         bool nt = f(ind+1,n,sum,coins,dp);
+        
         return dp[ind][sum] = t||nt;
     }
 
     int isPossible(int n, int coins[]) {
+        
         int sum = accumulate(coins,coins+n,0);
+        
         vector<vector<int>> dp(n,vector<int> (sum+1,-1));
+        
         return f(0,n,0, coins, dp);
     }
 };
